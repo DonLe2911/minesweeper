6b919e0592a47a6bc9f9e9ef7a23a8e2e738d455
donle2911 <donle@kainos.com>
2019-02-05 11:10:39 +0000

    dynamic board generation and bomb board generation with random bomb positions

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,7 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   let board = [];
   for (let i = 0; i < numberOfRows; i++) {
-    let row = [];
+    const row = [];
     for (let j = 0; j < numberOfColumns; j++) {
       row.push(' ');
     }
@@ -10,4 +10,38 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return(board);
 };
 
-console.log(generatePlayerBoard(4, 3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    const row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+let numberOfBombsPlaced = 0;
+while (numberOfBombsPlaced < numberOfBombs){
+  const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+  const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+  board[randomRowIndex][randomColumnIndex] = 'B';
+  numberOfBombsPlaced++;
+}
+return(board);
+};
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+const playerBoard = generatePlayerBoard (3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
+
+
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+
+console.log('Bomb Board: ');
+printBoard(bombBoard);
