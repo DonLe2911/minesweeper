937e7c6f58882eaa5c0f5e650b9feb8194c29a5f
donle2911 <donle@kainos.com>
2019-02-06 17:06:02 +0000

    minesweeper game logic, surroung tiles of bombs

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -20,13 +20,40 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     board.push(row);
   }
 
+const getNumberOfNeighbourBombs = (bombBoard, i, j) => {
+  neighbourOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ];
+  rows = bombBoard.length;
+  columns = bombBoard[0].length;
+  bombs = 0;
+  neighbourOffsets.forEach(offset => {
+    const neighbourRowIndex = i + offset[0];
+    const neighbourColumnIndex = j + offset[1];
+    if(neighbourRowIndex >= 0 && neighbourRowIndex < numberOfRows && neighbourColumnIndex >= 0 && neighbourColumnIndex < numberOfColumns) {
+      if(bombBoard[neighbourRowIndex][neighbourColumnIndex] === 'B') {
+        bombs += 1;
+      }
+    }
+    return numberOfBombs;
+  });
+}
+
 let numberOfBombsPlaced = 0;
 while (numberOfBombsPlaced < numberOfBombs){
   const randomRowIndex = Math.floor(Math.random() * numberOfRows);
   const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-
-  board[randomRowIndex][randomColumnIndex] = 'B';
-  numberOfBombsPlaced++;
+  if(board[randomRowIndex][randomColumnIndex] !== 'B') {
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
 }
 return(board);
 };
@@ -35,8 +62,8 @@ const printBoard = (board) => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
-const playerBoard = generatePlayerBoard (3, 4);
-const bombBoard = generateBombBoard(3, 4, 5);
+const playerBoard = generatePlayerBoard (3, 3);
+const bombBoard = generateBombBoard(3, 3, 5);
 
 
 
